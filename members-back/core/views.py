60b549cfcf4ebc21873ae450b1be2ac8137cd584
Vitor Costa TI <vitortecnologia62@gmail.com>
2022-05-07 16:42:32 -0300

    prepara primeiro endpoint

--- members-back/core/views.py
+++ members-back/core/views.py
@@ -1,9 +1,8 @@
 from rest_framework import viewsets
 from .models import Member
-from .serializers import Member
+from .serializers import MemberSerializer
 
 
 class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all()
-    serializers_class = MemberSerializer
-
+    serializer_class = MemberSerializer
