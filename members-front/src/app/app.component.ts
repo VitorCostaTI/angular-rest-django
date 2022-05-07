a26146bca244618e3e08b443bc01daaf38f3a1df
Vitor Costa TI <vitortecnologia62@gmail.com>
2022-05-07 18:24:44 -0300

    cria requisição no angular

--- members-front/src/app/app.component.ts
+++ members-front/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'members-front';
+
+  members = [
+    {name: 'Member 01' , id:1, surname: "Ciano", photo:'http://www.minhaapp.com/photo1'},
+    {name: 'Member 02' , id:2, surname: "Ciano", photo:'http://www.minhaapp.com/photo1'},
+    {name: 'Member 03' , id:3, surname: "Ciano", photo:'http://www.minhaapp.com/photo1'},
+  ];
+
+  constructor(private api:ApiService){
+    this.getMembers();
+  }
+
+  getMembers = () => {
+    this.api.getAllMembers().subscribe(
+      data => 
+        this.members = data
+    );
+  };
 }
