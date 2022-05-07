7a1e8d96f3aa759aee462014da8c5296403626ad
Vitor Costa TI <vitortecnologia62@gmail.com>
2022-05-07 15:00:03 -0300

    define model

--- members-back/core/models.py
+++ members-back/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    name =  models.CharField(max_length=30)
+    surname =  models.CharField(max_length=30)
+    phone =  models.CharField(max_length=30)
+    email = models.EmailField()
+    address = models.CharField(max_length=30)
+    photo = models.ImageField(upload_to='members_profile')
+
+    def __str__(self):
+        return self.name + " " + self.surname
